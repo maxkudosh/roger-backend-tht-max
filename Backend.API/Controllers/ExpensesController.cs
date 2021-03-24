7e408ccf6bad32300437c6034c827f1242ee2347
Max Kudosh <max_kudosh@hotmail.com>
2021-03-24 16:56:05 +0300

    Implement API version with neede operations, some filtering and dummy exchange provider

--- Backend.API/Controllers/ExpensesController.cs
+++ Backend.API/Controllers/ExpensesController.cs
@@ -1,18 +1,101 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Backend.Domain;
+using Backend.Foundation;
+
+using Common.Utils.ApiDataContracts;
 
 namespace Backend.API.Controllers
 {
-    /// <summary>
-    /// TODO: implement logic for both reading/writing expenses
-    /// </summary>
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ExpensesController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<string> Test()
+        private readonly IExpenseManagementService _expenseManagementService;
+
+
+        public ExpensesController(IExpenseManagementService expenseManagementService)
         {
-            return "I am alive!";
+            _expenseManagementService = expenseManagementService;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var expenses = await _expenseManagementService.GetAllExpenses();
+            var dataResponse = new ApiDataResponse<IReadOnlyCollection<Expense>>(expenses);
+
+            return Ok(dataResponse);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetByCreditor(Guid creditorId)
+        {
+            var expenses = await _expenseManagementService.GetExpensesByCreditorAsync(creditorId);
+            var dataResponse = new ApiDataResponse<IReadOnlyCollection<Expense>>(expenses);
+
+            return Ok(dataResponse);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetByCurrency(string currencyCode)
+        {
+            var expenses = await _expenseManagementService.GetExpensesByCurrencyAsync(currencyCode);
+            var dataResponse = new ApiDataResponse<IReadOnlyCollection<Expense>>(expenses);
+
+            return Ok(dataResponse);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            var getExpenseResult = await _expenseManagementService.GetExpense(id);
+
+            if (getExpenseResult.IsSuccessful)
+            {
+                var successResponse = new ApiDataResponse<Expense>(getExpenseResult.Data);
+
+                return Ok(successResponse);
+            }
+
+            var errorResponse = new ApiErrorResponse<GetExpenseErrorType>(getExpenseResult.ErrorType);
+
+            return BadRequest(errorResponse);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Create()
+        {
+            // TODO: define proper parameters.
+            var newExpenseRequest = new AddExpenseRequest(123123, "EUR", new byte[0], Guid.NewGuid());
+            var addExpenseResult = await _expenseManagementService.AddExpenseAsync(newExpenseRequest);
+
+            if (addExpenseResult.IsSuccessful)
+            {
+                var successResponse = new ApiResponse(addExpenseResult.IsSuccessful);
+
+                return Created("HERE LIES THE URI TO THE CREATED EXPENSE - NOT IMPLEMENTED YET :)", successResponse);
+            }
+
+            var errorResponse = new ApiErrorResponse<AddExpenseErrorType>(addExpenseResult.ErrorType);
+
+            return BadRequest(errorResponse);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            await _expenseManagementService.DeleteExpenseAsync(id);
+
+            var successResponse = new ApiResponse(true);
+
+            return NoContent();
         }
     }
 }
--- Backend.API/Startup.cs
+++ Backend.API/Startup.cs
@@ -1,23 +1,43 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using Backend.Storage.EntityFrameworkCore;
+using Backend.Storage;
+using Backend.Foundation;
+
+using Common.Currency;
+using Common.Currency.Fixer;
+using Common.Utils.DateTime;
 
 namespace Backend.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddEntityFrameworkSqlServer();
+            services.AddDbContext<ExpensesContext>(options => options.UseSqlServer("name=ConnectionStrings:ExpensesDatabase"));
+            services.AddScoped<IExpenseRepository, ExpenseRepository>();
+            services.AddScoped<IUnitOfWork, ExpensesUnitOfWork>();
+            services.AddSingleton<IDateTimeProvider, SystemDateTimeProvider>();
+            services.AddSingleton<DummyFixerCurrencyRatesProvider>();
+            services.AddSingleton<ICurrencyRatesProvider>(provider => new CachingCurrencyRatesProvider(provider.GetRequiredService<DummyFixerCurrencyRatesProvider>()));
+            services.AddSingleton<ICurrencyExchangeService, CurrencyExchangeService>();
+            services.AddScoped<IExpenseManagementService, ExpenseManagementService>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
